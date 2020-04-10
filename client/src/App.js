00cb7672cb291801e3591efbae913e7e76b983a7
Danial Hadavi <danialhadavi@gmail.com>
2020-04-10 12:23:35 -0400

    finished showing up my cards and added 2 custom hooks for stretch working on testing now

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,43 @@
 import React from "react";
+import axios from "axios";
+import { PlayerCard } from "./components/PlayerCard";
+import { NavigationBar } from "./components/NavigationBar";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then((response) => {
+        console.log("data", response);
+        this.setState({
+          players: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log("Data not received", error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <NavigationBar />
+        {this.state.players.map((player) => (
+          <PlayerCard
+            key={player.id}
+            name={player.name}
+            country={player.country}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
